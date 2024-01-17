b509ddc72c6739527f3b3ade1352c44ed2e477a1
NouranYasser <nouranyasser366@gmail.com>
2024-01-17 22:27:13 +0200

    update BG

--- routes/api.php
+++ routes/api.php
@@ -23,4 +23,12 @@ Route ::post('/addList',  [ListController ::class,'addList']);
 Route ::delete('/deleteList',  [ListController ::class,'deleteList']);
 Route ::get('/babyGrowth_1',[BabyGrowthController::class,'get_babyGrowth_1']);
 Route::get('/getListById/{user_id}', [ListController::class, 'getListById']);
-
+Route::get('/get_babyGrowth_1', [BabyGrowthController::class, 'get_babyGrowth_1']);
+Route::get('/get_babyGrowth_2', [BabyGrowthController::class, 'get_babyGrowth_2']);
+Route::get('/get_babyGrowth_3', [BabyGrowthController::class, 'get_babyGrowth_3']);
+Route::get('/get_babyGrowth_4', [BabyGrowthController::class, 'get_babyGrowth_4']);
+Route::get('/get_babyGrowth_5', [BabyGrowthController::class, 'get_babyGrowth_5']);
+Route::get('/get_babyGrowth_6', [BabyGrowthController::class, 'get_babyGrowth_6']);
+Route::get('/get_babyGrowth_7', [BabyGrowthController::class, 'get_babyGrowth_7']);
+Route::get('/get_babyGrowth_8', [BabyGrowthController::class, 'get_babyGrowth_8']);
+Route::get('/get_babyGrowth_9', [BabyGrowthController::class, 'get_babyGrowth_9']);
