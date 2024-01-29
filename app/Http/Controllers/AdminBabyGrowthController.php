7a3636616fb8e0c1036068216dd52bcecb408313
mostafafayez <fayezmostafa51@gmail.com>
2024-01-29 16:23:31 +0200

    update

--- app/Http/Controllers/AdminBabyGrowthController.php
+++ app/Http/Controllers/AdminBabyGrowthController.php
@@ -34,7 +34,7 @@ class AdminBabyGrowthController extends Controller
     
 
 
-    public function update_DESC(Request $req, $id)
+    public function update_all(Request $req, $id)
 {
     $description = Des_Categories::find($id);
 
@@ -89,30 +89,67 @@ public function get_DESC($id)
     ];
 }
 
-
 public function get_all_DESC()
 {
-    // Retrieve all records from the Des_Categories table
-    $descriptions = Des_Categories::all();
+    try {
+       
+        $descriptions = Des_Categories::all();
 
-    // Check if there are any records
-    if ($descriptions->isEmpty()) {
-        return ["Result" => "No records found"];
+       
+        $result = $descriptions->map(function ($description) {
+          
+            $filteredData = collect($description->toArray())->except(['created_at', 'updated_at']);
+
+            return $filteredData;
+        });
+
+        return response()->json($result, 200);
+    } catch (\Exception $e) {
+        return response()->json(["error" => $e->getMessage()], 500);
     }
-
-    // Transform the collection of records into an array of arrays
-    $result = $descriptions->map(function ($description) {
-        return [
-            "category_id" => $description->category_id,
-            "title" => $description->title,
-            "description" => $description->description,
-            "month" => $description->month,
-            "image" => $description->image,
-        ];
-    })->toArray();
-
-    return $result;
 }
 
 
+
+
+
+public function update_DESC(Request $req, $id)
+{
+    $description = Des_Categories::find($id);
+
+    if (!$description) {
+        return ["Result" => "Record not found"];
+    }
+
+    // Update only the fields that are provided in the request
+    if ($req->filled('category_id')) {
+        $description->category_id = $req->input('category_id');
+    }
+
+    if ($req->filled('title')) {
+        $description->title = $req->input('title');
+    }
+
+    if ($req->filled('description')) {
+        $description->description = $req->input('description');
+    }
+
+    if ($req->filled('month')) {
+        $description->month = $req->input('month');
+    }
+
+    $fileName = "";
+    if ($req->hasFile('image')) {
+        $fileName = $req->file('image')->store('posts', 'public');
+        $description->image = $fileName;
+    }
+
+    $result = $description->save();
+
+    if ($result) {
+        return ["Result" => "Updated Successfully"];
+    } else {
+        return ["Result" => "There is something wrong"];
+    }
+}
 }
